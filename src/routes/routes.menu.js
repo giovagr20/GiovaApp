82eaaa77f5010043145eb40a93ba5693b2f761c5
giovagr20 <giovannyg32@gmail.com>
2020-02-10 15:05:04 -0500

    About services all complete

--- src/routes/routes.menu.js
+++ src/routes/routes.menu.js
@@ -1,11 +1,11 @@
 const {Router} = require('express');
 const router = Router();
 
-router.get('/en', (req, res)=>{
+router.get('/', (req, res)=>{
     res.render('menu_english');
 });
 
-router.get('/', (req,res)=>{
+router.get('/es', (req,res)=>{
     res.render('menu_spanish');
 });
 
