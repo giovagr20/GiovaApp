8d886423eda89bdeb46baaa668e6c11821e96d71
Giovanni G R <giovannyg32@gmail.com>
2021-01-08 16:48:01 -0500

    Se añaden modificaciones en envio mensaje

--- src/controller/controller.send_email.js
+++ src/controller/controller.send_email.js
@@ -4,7 +4,19 @@ const _sendData = require('../models/model.send_email');
 
 _sendEmail.getMessageSpanish = (req, res) => {
 
-  res.send(req.body);
+  let name = req.body.inputNameEn;
+  let email = req.body.inputEmailEn;
+  let subject = req.body.inputSubjectEn;
+  let description = req.body.areaMessageEn;
+
+  if((name === null || name ==="") &&
+    (email === "" || email === null)&&
+    (subject === "" || subject === null) &&
+    (description === "" || description===null)) {
+      res.send(`<h1 class="alert alert-danger" role="alert"> Debes llenar la totalidad de campos </h1>`)
+    }
+
+  //{"inputNameEn":"","inputEmailEn":"","inputSubjectEn":"","areaMessageEn":""}
   
 
   /*  if (!inputNameEs) {
@@ -15,7 +27,22 @@ _sendEmail.getMessageSpanish = (req, res) => {
 
 
 _sendEmail.getMessageEnglish = (req, res) => {
-  res.send(req.body);
+
+  let name = req.body.inputNameEn;
+  let email = req.body.inputEmailEn;
+  let subject = req.body.inputSubjectEn;
+  let description = req.body.areaMessageEn;
+
+  if((name === null || name ==="") &&
+    (email === "" || email === null)&&
+    (subject === "" || subject === null) &&
+    (description === "" || description===null)) {
+      res.send(`<h1 class="alert alert-danger" role="alert"> Debes llenar la totalidad de campos </h1>
+      <a src="/en" class="btn btn-primary">Regresar</a>`)
+    } else {
+      res.send(`<h1 class="alert alert-success" role="alert"> Se envío mensaje</h1> 
+      <a src="/en" class="btn btn-primary">Regresar</a>`)
+    }
 
   /*if (!inputNameEs) {
       res.redirect('/error');
