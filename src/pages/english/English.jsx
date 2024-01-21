a9e45a60206e8a782aa8cfa61098ce2d59c91fdd
Giovanni Gomez Restrepo <giovannyg32@gmail.com>
2024-01-21 08:14:52 -0500

    It was deleted projects

--- src/pages/english/English.jsx
+++ src/pages/english/English.jsx
@@ -2,7 +2,6 @@ import { Idiom } from "../../utils/utils.language";
 import { WhoAmI } from "../../components/whoami/WhoAmI";
 import { Skills } from "../../components/skills/Skill";
 import { Services } from "../../components/services/Service";
-import { Projects } from "../../components/projects/Project";
 
 export const English = () => {
   return (
@@ -10,7 +9,6 @@ export const English = () => {
         <WhoAmI idiom = {Idiom.english}/>
         <Skills idiom = {Idiom.english}/>
         <Services idiom = {Idiom.english}/>
-        <Projects idiom = {Idiom.english}/>
     </>
   );
 };
--- src/pages/spanish/Spanish.jsx
+++ src/pages/spanish/Spanish.jsx
@@ -1,7 +1,6 @@
 import { WhoAmI } from "../../components/whoami/WhoAmI";
 import { Skills } from "../../components/skills/Skill";
 import { Services } from "../../components/services/Service";
-import { Projects } from "../../components/projects/Project";
 import { Idiom } from "../../utils/utils.language";
 
 export const Spanish = () => {
@@ -10,7 +9,6 @@ export const Spanish = () => {
         <WhoAmI idiom = {Idiom.spanish} />
         <Skills idiom = {Idiom.spanish} />
         <Services idiom = {Idiom.spanish} />
-        <Projects idiom = {Idiom.spanish} />
     </>
   );
 };
